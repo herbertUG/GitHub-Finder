e5350d23b8b474f569cc60ecd56c8e1a94ab92dc
herbert bruno <herbertbruno@pop-os.localdomain>
2019-12-03 05:17:14 +0300

    show repos

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -43,6 +44,15 @@ class App extends Component {
     );
 
     this.setState({ user: res.data, loading: false });
+  };
+  // get user repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
   };
   // clear users from state
   clearUsers = () => {
@@ -87,7 +97,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={this.state.user}
+                    repos={this.state.repos}
                     loading={this.state.loading}
                   />
                 )}
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -2,15 +2,19 @@ import React, { Component, Fragment } from 'react';
 import spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Repos from '../repos/Repos';
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
   static propTypes = {
     laoding: PropTypes.bool,
     user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
+    repos: PropTypes.array.isRequired
   };
   render() {
     const {
@@ -28,7 +32,7 @@ class User extends Component {
       public_gists,
       hirable
     } = this.props.user;
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
     if (loading) return <spinner />;
     return (
       <Fragment>
@@ -89,10 +93,11 @@ class User extends Component {
         </div>
         <div className="card text-center">
           <div className="badge badge-primary">Followers: {followers}</div>
-          <div className="badge badge-success">Followers: {following}</div>
-          <div className="badge badge-light">Followers: {public_repos}</div>
-          <div className="badge badge-dark">Followers: {public_gists}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+          <div className="badge badge-dark">Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
